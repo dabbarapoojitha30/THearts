ca2efcd7e3aa174f6e82739b30e82f7dc93dea2a
Poojitha Dabbara <dabbarapoojitha30@gmail.com>
2026-02-20 21:42:46 +0530

    Initial commit - new project

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// Detect Render environment
-const isRender = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -10,10 +9,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: isRender ? { rejectUnauthorized: false } : false // SSL for Render, false for local
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
-// Test DB connection
 pool.query('SELECT current_database();')
   .then(res => console.log("✅ Connected to database:", res.rows[0].current_database))
   .catch(err => console.error("❌ DB connection error:", err));
