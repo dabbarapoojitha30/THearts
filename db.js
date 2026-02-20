bd3d7959be911358d52d613343584309bde5256c
Poojitha Dabbara <dabbarapoojitha30@gmail.com>
2026-02-21 00:32:45 +0530

    FIXED

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const { Pool } = require("pg");
 
 if (!process.env.DATABASE_URL) {
-  console.error("❌ DATABASE_URL not found!");
+  console.error("❌ DATABASE_URL not found! Exiting.");
   process.exit(1);
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // REQUIRED for Render
+  ssl: { rejectUnauthorized: false } // required for Render Postgres
 });
 
 pool.connect()
-  .then(() => console.log("✅ DB connected (Render Postgres)"))
+  .then(() => console.log("✅ DB connected"))
   .catch(err => console.error("❌ DB connection error:", err));
 
 module.exports = pool;
