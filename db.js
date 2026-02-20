f72de8b783b08a32005d68d619032c3b1ac5c716
Poojitha Dabbara <dabbarapoojitha30@gmail.com>
2026-02-20 23:01:30 +0530

    Initial commit - new project

--- db.js
+++ db.js
@@ -2,14 +2,8 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER?.trim(),
-  host: process.env.DB_HOST?.trim(),
-  database: process.env.DB_NAME?.trim(),
-  password: process.env.DB_PASSWORD?.trim(),
-  port: Number(process.env.DB_PORT?.trim()),
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.query('SELECT current_database();')
